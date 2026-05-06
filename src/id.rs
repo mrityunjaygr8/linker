63565931f458fbad1248ccb7d4399a50602508b7
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2026-05-06 12:31:37 +0530

    test added to id generate

--- src/id.rs
+++ src/id.rs
@@ -4,3 +4,14 @@ pub fn generate() -> String {
     nanoid!(10)
     // "woo-123".to_string()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_generate_length() {
+        let id = generate();
+        assert_eq!(10, id.len())
+    }
+}
