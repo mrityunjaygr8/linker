fb7a468c9b53103182675ce15a6dc11e85999b66
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2026-05-06 12:29:14 +0530

    tests and PartialEq added to ParsedURL and URLParseError

--- src/url.rs
+++ src/url.rs
@@ -2,7 +2,7 @@ use std::fmt::Display;
 
 use url::Url;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum URLParseError {
     NotValidError(String),
 }
@@ -15,7 +15,7 @@ impl Display for URLParseError {
     }
 }
 
-#[derive(Debug, Default, Clone)]
+#[derive(Debug, Default, Clone, PartialEq)]
 pub struct ParsedURL(String);
 impl ParsedURL {
     pub fn parse(value: String) -> Result<Self, URLParseError> {
@@ -35,5 +35,44 @@ impl TryFrom<String> for ParsedURL {
     type Error = URLParseError;
     fn try_from(value: String) -> Result<Self, Self::Error> {
         ParsedURL::parse(value)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_url_parse_correct() {
+        let text = "http://google.com".to_string();
+        let url = ParsedURL::parse(text.clone());
+        let parsed = url.unwrap();
+        assert_eq!(text, parsed.0)
+    }
+
+    #[test]
+    fn test_url_parse_fail() {
+        let text = "test.com".to_string();
+        let url = ParsedURL::parse(text.clone());
+
+        let err = url.unwrap_err();
+        let expected_error = URLParseError::NotValidError(text.clone());
+        assert_eq!(err, expected_error);
+    }
+
+    #[test]
+    fn test_url_from_string() {
+        let text = "http://test.com".to_string();
+        let expected = ParsedURL { 0: text.clone() };
+
+        let tested: ParsedURL = text.try_into().unwrap();
+        assert_eq!(expected, tested);
+    }
+
+    #[test]
+    fn test_url_parse_error_display() {
+        let text = "test.com".to_string();
+        let e = URLParseError::NotValidError(text.clone());
+        assert_eq!(format!("{}", e), format!("{}: URL is not valid", text))
     }
 }
